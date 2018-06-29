e1bc13c13f53db2bc091d169c1170169fa544b2e
Luego 08 - Rodrigo de Melo <rodrigodemelo@luego.com.br>
2018-06-29 17:47:53 -0300

    Working...

--- src/http/routes.js
+++ src/http/routes.js
@@ -7,7 +7,7 @@ const routes = (server) => {
   });
 
   server.get('/todo', (req, res, next) => {
-    db.todos().all().then(todos => {
+    db.todos().all().then((todos) => {
       res.send(todos);
       next();
     });
@@ -16,7 +16,26 @@ const routes = (server) => {
   server.post('/todo', (req, res, next) => {
     const { name } = req.params;
 
-    db.todos().save(name).then(todos => {
+    db.todos().save(name).then((todo) => {
+      res.send(todo);
+      next();
+    });
+  });
+
+  server.put('/todo', (req, res, next) => {
+    const { id, name } = req.params;
+
+    db.todos().update(id, name).then((todo) => {
+      res.send(todo);
+      next();
+    });
+  });
+
+  server.del('/todo', (req, res, next) => {
+    const { id } = req.params;
+
+    db.todos().del(id).then((todo) => {
+      res.send(todo);
       next();
     });
   });
--- src/services/mysql/todos.js
+++ src/services/mysql/todos.js
@@ -20,7 +20,7 @@ const todos = (deps) => {
 
         connection.query('INSERT INTO todo (name) VALUES (?)', [name], (error, results) => {
           if (error) {
-            errorHandler(error, 'falha ao salvar o todo', reject);
+            errorHandler(error, `falha ao salvar o todo ${name}`, reject);
             return false;
           }
 
@@ -28,8 +28,34 @@ const todos = (deps) => {
         });
       });
     },
-    update: (id, name) => {},
-    del: (id) => {},
+    update: (id, name) => {
+      return new Promise((resolve, reject) => {
+        const { connection, errorHandler } = deps;
+
+        connection.query('UPDATE todo SET name = ? WHERE id = ?', [name, id], (error, results) => {
+          if (error) {
+            errorHandler(error, `falha ao atualizar o todo ${name}`, reject);
+            return false;
+          }
+
+          resolve({ todo: { name } });
+        });
+      });
+    },
+    del: (id) => {
+      return new Promise((resolve, reject) => {
+        const { connection, errorHandler } = deps;
+
+        connection.query('DELETE from todo WHERE id = ?', [id], (error, results) => {
+          if (error) {
+            errorHandler(error, `falha ao remover o todo de id: ${id}`, reject);
+            return false;
+          }
+
+          resolve({ todo: { id } });
+        });
+      });
+    },
   }
 }
 
